c5bb8e6492b91fb3a1573fddc6ce1614b1d8899e
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-02-13 21:33:50 +0530

    tailwind for design

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,11 +3,13 @@ import React from "react";
 function Filter({discriminativeData})
 {
     return(
-        <div>
+        <div className="w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center">
          {discriminativeData.map((indivisualFilter)=>
          {
           return (
-          <button key={indivisualFilter.id}>
+          <button className="text-lg px-4 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50
+          border-2 transition-all duration-100"
+          key={indivisualFilter.id}>
           {indivisualFilter.title}
           </button>);
          })}  
